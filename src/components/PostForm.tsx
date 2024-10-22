89128ccff64961bf815dedc538104e329493a80b
Md. Ayub Shamim <ashamim@macg.com>
2024-10-22 19:04:23 +0600

    [refactor] Add space between label

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -41,7 +41,7 @@ const PostForm: React.FC<PostFormProps> = ({ onSubmit, initialPost }) => {
 
                 <Box className="w-96">
                     <Box className="relative w-full min-w-[200px]">
-                        <h1>Body</h1>
+                        <h1 className='mt-2'>Body</h1>
                         <textarea
                             className="h-full mt-0.5 min-h-[100px] w-full resize-none rounded-[7px] border border-slate-400 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:outline-0 disabled:resize-none disabled:bg-blue-gray-50"
                             placeholder=" "
